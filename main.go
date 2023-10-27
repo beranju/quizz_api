a788199a210bffd14d68cd93a47e34fff1888b36
beranju <beranjusihombing1@gmail.com>
2023-10-27 20:17:06 +0700

    add  middleware

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"main/config"
 	"main/routes"
+
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+	// config.LoadEnv()
 	config.InitDb()
 	e := routes.New()
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.Timeout())
 	e.Logger.Fatal(e.Start(":8080"))
 }
