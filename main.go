b32d8b4c64339d1450b4b1c7790c30f166d40ebb
beranju <beranjusihombing1@gmail.com>
2023-10-28 01:00:43 +0700

    add jwt to routes

--- main.go
+++ main.go
@@ -3,19 +3,11 @@ package main
 import (
 	"main/config"
 	"main/routes"
-	"time"
-
-	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	// config.LoadEnv()
+	config.LoadEnv()
 	config.InitDb()
 	e := routes.New()
-	e.Pre(middleware.RemoveTrailingSlash())
-	e.Use(middleware.TimeoutWithConfig(middleware.TimeoutConfig{
-		Timeout: 30 * time.Second,
-	}))
-	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(20)))
 	e.Logger.Fatal(e.Start(":8080"))
 }
