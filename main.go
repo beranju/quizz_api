5c08f6d9e42675cd727132654f77329c361c51d6
beranju <beranjusihombing1@gmail.com>
2023-10-30 16:00:24 +0700

    update env post

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"main/config"
 	"main/routes"
+	"os"
 )
 
 func main() {
 	// config.LoadEnv()
 	config.InitDb()
 	e := routes.New()
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(envPortOr("8000")))
+}
+
+func envPortOr(port string) string {
+	envPort := os.Getenv("PORT")
+	if envPort != "" {
+		return ":" + envPort
+	}
+	return ":" + port
 }
